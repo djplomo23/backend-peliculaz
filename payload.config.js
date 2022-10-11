ea7f4778d07c162f2a250607830db66b17f37912
djplomo23 <69323758+djplomo23@users.noreply.github.com>
2022-10-11 00:18:15 -0400

    probando el serverUrl

--- payload.config.js
+++ payload.config.js
@@ -5,7 +5,7 @@ import Users from './collections/Users';
 
 
 export default buildConfig({
-  serverURL: 'http://localhost:3000',
+  //serverURL: 'http://localhost:3000',
  
   admin: {
     user: Users.slug,
